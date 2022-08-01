fbf31bf3b79021383ae92a747eff329bf47e7035
Jan Imhof <j.imhof@domologic.de>
2022-08-01 16:23:52 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(f"{dirname(__file__)}/requirements.txt", "r") as f:
 
 setup(
     name='wikijspy-ext',
-    version='0.0.2',
+    version='0.0.3',
     author='WERTBON1789',
     author_email='jan210404@gmx.de',
     packages=find_packages(),
